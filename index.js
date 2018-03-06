70e3ec08ef6fc0e307ab2b1e8c8d0c90d51b8feb
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-03-06 01:31:58 +0100

    generalized for 1st version

--- index.js
+++ index.js
@@ -5,25 +5,16 @@ var rxFromMysql = require('./rx-from-mysql');
 var config = {
     host: '192.168.50.144',
     user: 'root',
-    password: ''
+    password: 'yourPassword'
 };
 
-const sql100Articles = 'select * from web.articulos limit 100';
+var sql100Articles = 'select * from web.articulos limit 100';
 
 var rowDataPacket$ = rxFromMysql(sql100Articles, config);
 
 rowDataPacket$
-    .map(function (next) {
-        return {
-            idArticulo: next['id_articulo'],
-            nombreArticulo: next['nombre'],
-        };
-    })
-    .filter(
-        function (next) {
-            return next.idArticulo < 1000;
-        }
-    )
+    //.map
+    //.filter
     .subscribe(
         function next(nextRowDataPacket) {
             console.log('next', nextRowDataPacket)
